b0819956a7699210fc39b934b49dffd07cde5310
shwaoexol <sherova1994@gamil.com>
2026-06-22 16:53:01 +0500

    fin

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,5 +1,11 @@
 import { createClient } from "@supabase/supabase-js";
 
+console.log("SUPABASE_URL =", process.env.NEXT_PUBLIC_SUPABASE_URL);
+console.log(
+  "SUPABASE_KEY EXISTS =",
+  !!process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+);
+
 export const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
